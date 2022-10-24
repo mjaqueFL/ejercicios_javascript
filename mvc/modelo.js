2e740e9517b4858db642043b018130c335e3778a
mjaqueFL <61022525+mjaqueFL@users.noreply.github.com>
2022-10-24 12:19:16 +0200

    Añadidos comentarios para JDoc

--- mvc/modelo.js
+++ mvc/modelo.js
@@ -1,8 +1,25 @@
+/**
+	@file Contiene el modelo de la aplicación
+	@author Miguel Jaque <mjaque@fundacionloyola.es>
+	@license GPL-3.0-or-later
+**/
+
+/**
+	Clase Modelo
+	Gestiona los datos de la aplicación.
+**/
 export class Modelo{
+	/**
+		Constructor de la clase
+	**/
 	constructor(){
 		this.lista = []
 	}
-
+	/**
+		Inserta un nuevo registro.
+		@param nombre {String} Atributo nombre del nuevo registro
+		@param email {String} Atributo email del nuevo registro
+	**/
 	insertar(nombre, email){
 		this.lista.push({'nombre': nombre, 'email': email})
 	}
