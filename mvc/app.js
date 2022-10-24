d7183926172c4a54eba39410f88d4d37f07e9cba
mjaqueFL <61022525+mjaqueFL@users.noreply.github.com>
2022-10-24 12:17:28 +0200

    Añadidos comentarios para JSDoc

--- mvc/app.js
+++ mvc/app.js
@@ -1,3 +1,8 @@
+/**
+	@file Contiene el controlador principal de la aplicación
+	@author Miguel Jaque <mjaque@fundacionloyola.es>
+	@license GPL-3.0-or-later
+**/
 import {Modelo} from './modelo.js'
 
 import {VistaNav} from './vistanav.js'
@@ -7,9 +12,17 @@ import {VistaCRUD} from './vistacrud.js'
  * Controlador de la aplicación
  */
 class Controlador{
+	/**
+		Constructor de la clase
+		Llama al método iniciar al cargarse la página
+	**/
 	constructor(){
 		window.onload = this.iniciar.bind(this)
 	}
+	/**
+		Inicia la aplicación
+		Crea el modelo y las vistas.
+	**/
 	iniciar(){
 		this.modelo = new Modelo()
 
@@ -23,10 +36,19 @@ class Controlador{
 
 		this.vistaInicio.mostrar(true)
 	}
+	/**
+		Atención a la pulsación del enlace al CRUD en el menú de navegación.
+	**/
 	pulsarNavCRUD(){
 		this.vistaInicio.mostrar(false)
 		this.vistaCRUD.mostrar(true)
 	}
+	/**
+		Atención al click en el botón Aceptar del CRUD.
+		Inserta el elemento en el modelo.
+		@param nombre {String} Nombre del elemento a registrar
+		@param email {String} Correo electrónico del elemento a registrar
+	**/
 	aceptarCRUD(nombre, email){
 		this.modelo.insertar(nombre, email)
 	}
