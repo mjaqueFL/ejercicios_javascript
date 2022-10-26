9014c085b8181000f1365d8c50707ed69118c172
Miguel Jaque <mjaque@migueljaque.com>
2022-10-26 08:11:55 +0200

    Añadida documentación de clases
    Preparadas para aplicar JSDoc.

--- mvc/vistanav.js
+++ mvc/vistanav.js
@@ -1,4 +1,18 @@
+/**
+	@file Contiene el modelo de la vista de menú de navegación de la aplicación
+	@author Miguel Jaque <mjaque@migueljaque.com>
+	@license GPL-3.0-or-later
+**/
+
+/**
+	Implementa una vista del menú de navegación.
+**/
 export class VistaNav{
+	/**
+		Constructor de la clase.
+		@param controlador {Controlador} Controlador de la vista.
+		@param nav {HtmlNavElement} Nav de HTML en el que se desplegará la vista.
+	**/
 	constructor(controlador, nav){
 		this.controlador = controlador
 		this.nav = nav
@@ -6,6 +20,9 @@ export class VistaNav{
 
 		this.liCRUD.onclick = this.pulsarCRUD.bind(this)
 	}
+	/**
+		Atención a la pulsación sobre el enlace de CRUD
+	**/
 	pulsarCRUD(){
 		this.controlador.pulsarNavCRUD()
 	}
