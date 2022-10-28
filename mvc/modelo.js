8ecbe7e8c5bf61a33e77c4bb7af88bc77ca04b05
mjaqueFL <61022525+mjaqueFL@users.noreply.github.com>
2022-10-28 20:38:19 +0200

    update modelo.js

--- mvc/modelo.js
+++ mvc/modelo.js
@@ -16,11 +16,26 @@ export class Modelo{
 		this.lista = []
 	}
 	/**
+	 * Registra un objeto para informarle de los cambios en el Modelo
+	 * @param {Function} Función de callback que será llamada cuando cambien los datos
+	 **/
+	registrar(callback){
+        this.lista.push(callback)
+	}
+	/**
+	 * Ejecuta todos los callback registrados.
+	 **/
+	 avisar(){
+	     for(let callback of this.lista)
+	        callback()
+	 }
+	/**
 		Inserta un nuevo registro.
 		@param nombre {String} Atributo nombre del nuevo registro
 		@param email {String} Atributo email del nuevo registro
 	**/
 	insertar(nombre, email){
 		this.lista.push({'nombre': nombre, 'email': email})
+		this.avisar()
 	}
 }
